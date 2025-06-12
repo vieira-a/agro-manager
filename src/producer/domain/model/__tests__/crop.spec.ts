5ab7dbb0f720466d294afa53fdb2a5ad4b7ae34e
Anderson Vieira <asvieira.dev@gmail.com>
2025-06-11 23:35:24 -0300

    test: add unit test for Crop creation with empty name

--- src/producer/domain/model/__tests__/crop.spec.ts
+++ src/producer/domain/model/__tests__/crop.spec.ts
@@ -1,3 +1,4 @@
+import { UnprocessableEntityException } from '@nestjs/common';
 import { Crop } from '../crop';
 
 describe('Crop', () => {
@@ -6,5 +7,11 @@ describe('Crop', () => {
   it('should create a crop successfully with a valid name', () => {
     const crop = Crop.create({ name: validName });
     expect(crop).toBeInstanceOf(Crop);
+  });
+
+  it('should throw if name is empty', () => {
+    expect(() => Crop.create({ name: '' })).toThrow(
+      UnprocessableEntityException,
+    );
   });
 });
