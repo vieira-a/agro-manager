0108116670863116fc4fb67c65ddc165a6d4b199
Anderson Vieira <asvieira.dev@gmail.com>
2025-06-12 01:24:36 -0300

    test: add unit tests for CPF normalization

---
+++ src/producer/domain/model/__tests__/cpf.spec.ts
@@ -0,0 +1,18 @@
+import { CPF } from '../cpf';
+import { DocumentValidator } from '../document-validator.interface';
+
+describe('CPF', () => {
+  const validCPF = '123.456.789-09';
+  const normalizedCPF = '12345678909';
+
+  const mockValidator: DocumentValidator = {
+    validate: jest.fn(() => true),
+    format: jest.fn((value) => `formatted-${value}`),
+  };
+
+  it('should normalize the CPF value on construction', () => {
+    const cpf = new CPF(validCPF, mockValidator);
+    expect(cpf.toString()).toBe(normalizedCPF);
+    expect(mockValidator.validate).toHaveBeenCalledWith(normalizedCPF);
+  });
+});
