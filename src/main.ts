b9d5cf11f37f7a671e0b312ccddfb8520152d81e
Anderson Vieira <asvieira.dev@gmail.com>
2025-06-13 10:26:08 -0300

    feat: set up Swagger documentation for the Agro Manager API

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { GlobalExceptionFilter } from './shared/exception/global-exception.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Agro Manager')
+    .setDescription('Sistema para gereciamento de propriedades rurais')
+    .setVersion('1.0')
+    .addTag('Endpoints')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/v1/docs', app, documentFactory);
 
   app.useGlobalFilters(new GlobalExceptionFilter());
   app.setGlobalPrefix('api/v1');
