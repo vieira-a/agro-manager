f93316aed743cdc77c1f46a944c68632817e5009
Anderson Vieira <asvieira.dev@gmail.com>
2025-06-13 10:08:09 -0300

    feat: set global API prefix to 'api/v1'

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalFilters(new GlobalExceptionFilter());
+  app.setGlobalPrefix('api/v1');
 
   await app.listen(process.env.PORT ?? 3000);
 }
