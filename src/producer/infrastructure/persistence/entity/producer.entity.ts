52a1c55869ba4ca065d953ff820abf1c02cbee0d
Anderson Vieira <asvieira.dev@gmail.com>
2025-06-30 15:55:51 -0300

    feat: implement JWT authentication with refresh tokens and role management

--- src/producer/infrastructure/persistence/entity/producer.entity.ts
+++ src/producer/infrastructure/persistence/entity/producer.entity.ts
@@ -1,6 +1,6 @@
 import { Column, Entity, OneToMany, PrimaryColumn } from 'typeorm';
 import { FarmEntity } from './farm.entity';
-import { ProducerRole } from 'src/producer/domain/enum/producer-role.enum';
+import { ProducerRole } from '../../../../producer/domain/enum/producer-role.enum';
 
 @Entity({ name: 'producers' })
 export class ProducerEntity {
